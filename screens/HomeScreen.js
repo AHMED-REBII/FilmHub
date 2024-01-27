f0566bf498fd51368dafc5134b9442a9798c89a1
Ahmed Rebii <ahmedrebai2001@gmail.com>
2024-01-27 09:23:30 +0100

    Home Screen UI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,52 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Platform,
+  TouchableOpacity,
+  ScrollView,
+  useAnimatedValue,
+} from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
+import TrendingMovies from "../components/trendingMovies";
+import MovieList from "../components/movieList";
+const ios = Platform.OS == "ios";
 
 export default function HomeScreen() {
-return (
-    <SafeAreaView>
-    <Text>HomeScreen</Text>
-    </SafeAreaView>)
+    const [trending, setTrending] = useState([1,2,3])
+    const [upcoming, setUpcoming] = useState([1,2,3])
+    const [topRated, setTopRated] = useState([1,2,3])
+  return (
+    <View className="flex-1 bg-neutral-800">
+      <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+        <StatusBar style="light" />
 
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size={30} strokeWidth={2} color="white" />
+
+          <Text className="text-white text-3xl font-bold">
+            Film<Text style={styles.text}>Hub</Text>
+          </Text>
+
+          <TouchableOpacity onPress={() => navigation.navigate("Search")}>
+            <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
+      >
+        <TrendingMovies data={trending} />
+        <MovieList title="Upcoming" data={upcoming}/>
+        <MovieList title="Top Rated" data={topRated}/>
+      </ScrollView>
+    </View>
+  );
 }
