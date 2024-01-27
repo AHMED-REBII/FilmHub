3328cce042d7efb5fb57cfd2523d27367a084cf3
Ahmed Rebii <ahmedrebai2001@gmail.com>
2024-01-27 16:22:39 +0100

    Movie Screen UI

--- components/movieList.js
+++ components/movieList.js
@@ -12,18 +12,19 @@ import { useNavigation } from "@react-navigation/native";
 import { styles } from "../theme";
 const { width, height } = Dimensions.get("window");
 
-export default function MovieList({ title, data }) {
+export default function MovieList({ title, data, hideSeeAll }) {
   const navigation = useNavigation();
   return (
     <View className="mb-8 space-y-4">
       <View className="mx-4 flex-row justify-between items-center">
         <Text className="text-white text-lg">{title}</Text>
-
-        <TouchableOpacity>
-          <Text style={styles.text} className="text-lg">
-            See All
-          </Text>
-        </TouchableOpacity>
+        {!hideSeeAll && (
+          <TouchableOpacity>
+            <Text style={styles.text} className="text-lg">
+              See All
+            </Text>
+          </TouchableOpacity>
+        )}
       </View>
 
       <ScrollView
@@ -45,9 +46,7 @@ export default function MovieList({ title, data }) {
                   className="rounded-3xl"
                   style={{ width: width * 0.33, height: height * 0.22 }}
                 />
-                <Text className="text-neutral-300 ml-1">
-                  choufli hal
-                </Text>
+                <Text className="text-neutral-300 ml-1">choufli hal</Text>
               </View>
             </TouchableWithoutFeedback>
           );
--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,14 +1,119 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Cast from "../components/cast";
+import MovieList from "../components/movieList";
 
-const MovieScreen = () => {
+import { styles, theme } from "../theme";
+
+const ios = Platform.OS == "ios";
+
+const topMargin = ios ? "" : " mt-3";
+
+var { width, height } = Dimensions.get("window");
+
+export default function MovieScreen() {
+  const { params: item } = useRoute();
+  const navigation = useNavigation();
+  const [movie, setMovie] = useState({});
+  const [cast, setCast] = useState([1, 2, 3, 4, 5]);
+  const [similarMovies, setSimilarMovies] = useState([1, 2, 3, 4, 5]);
+  const [isFavourite, toggleFavourite] = useState(false);
+
+  let movieName = "Ant-Man and the Wasp : Quantumania";
+
+  useEffect(() => {}, [item]);
+
   return (
-    <View>
-      <Text>MovieScreen</Text>
-    </View>
-  )
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 20 }}
+      className="flex-1 bg-neutral-900"
+    >
+      <View className="w-full">
+        <SafeAreaView
+          className={
+            "absolute z-20 w-full flex-row justify-between items-center px-4 " +
+            topMargin
+          }
+        >
+          <TouchableOpacity
+            style={styles.background}
+            className="rounded-xl p-1"
+            onPress={() => navigation.goBack()}
+          >
+            <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+            <HeartIcon
+              size="35"
+              color={isFavourite ? theme.background : "white"}
+            />
+          </TouchableOpacity>
+        </SafeAreaView>
+
+        <View>
+          <Image
+            // source={require('../assets/images/moviePoster2.png')}
+            // source={{uri: image500(movie.poster_path) || fallbackMoviePoster}}
+            source={require("../assets/images/moviePoster2.png")}
+            style={{ width, height: height * 0.55 }}
+          />
+          <LinearGradient
+            colors={[
+              "transparent",
+              "rgba(23, 23, 23, 0.8)",
+              "rgba(23, 23, 23, 1)",
+            ]}
+            style={{ width, height: height * 0.4 }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+            className="absolute bottom-0"
+          />
+        </View>
+      </View>
+      <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+        <Text className="text-white text-center text-3xl font-bold tracking-widest">
+          movieName
+        </Text>
+
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Released * 2020 * 170 min
+        </Text>
+
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Action *
+        </Text>
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Thrill *
+        </Text>
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Comedy *
+        </Text>
+        <Text className="text-neutral-400 mx-4 tracking-wide">
+          Description nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+        </Text>
+      </View>
+
+      <Cast cast={cast} navigation={navigation} />
+
+      <MovieList
+        title={"Similar Movies"}
+        hideSeeAll={true}
+        data={similarMovies}
+      />
+    </ScrollView>
+  );
 }
-
-export default MovieScreen
-
-const styles = StyleSheet.create({})
