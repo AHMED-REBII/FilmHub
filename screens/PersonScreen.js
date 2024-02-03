ffa8c596dc0681ad7c74e4375942a2e0c1c1ffc9
Ahmed Rebii <ahmedrebai2001@gmail.com>
2024-02-03 21:06:56 +0100

    Person Screen UI

---
+++ screens/PersonScreen.js
@@ -0,0 +1,106 @@
+import {
+  Dimensions,
+  Platform,
+  ScrollView,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { styles } from "../theme";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import MovieList from "../components/movieList";
+
+var { width, height } = Dimensions.get("window");
+const ios = Platform.OS == "ios";
+const verticalMargin = ios ? "" : " my-3";
+
+export default function PersonScreen() {
+  const [isFavourite, toggleFavourite] = useState(false);
+  const navigation = useNavigation();
+  const [personMovies, setPersonMovies] = useState([1, 2, 3, 4]);
+
+  return (
+    <ScrollView
+      className="flex-1 bg-neutral-900"
+      contentContainerStyle={{ paddingBottom: 20 }}
+    >
+      <SafeAreaView
+        className={
+          " z-20 w-full flex-row justify-between items-center px-4 " +
+          verticalMargin
+        }
+      >
+        <TouchableOpacity
+          style={styles.background}
+          className="rounded-xl p-1"
+          onPress={() => navigation.goBack()}
+        >
+          <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+          <HeartIcon size="35" color={isFavourite ? "red" : "white"} />
+        </TouchableOpacity>
+      </SafeAreaView>
+
+      <View>
+        <View
+          className="flex-row justify-center rounded-full w-72   "
+          style={{
+            shadowColor: "white",
+            shadowRadius: 40,
+            shadowOffset: { width: 0, height: 5 },
+            shadowOpacity: 1,
+            elevation: 15,
+            alignSelf: "center",
+          }}
+        >
+          <View className="items-center rounded-full overflow-hidden h-72 w-72 border-neutral-500 border-2">
+            <Image
+              source={require("../assets/images/castImage2.png")}
+              style={{ height: height * 0.49, width: width * 0.74 }}
+            />
+          </View>
+        </View>
+        <View className="mt-6">
+          <Text className="text-3xl text-white font-bold text-center">
+            Keanu Reeves
+          </Text>
+          <Text className="text-neutral-500 text-base text-center">
+            Beirut, Lebanon
+          </Text>
+        </View>
+
+        <View className="mx-3 p-4 mt-6 flex-row justify-between items-center bg-neutral-700 rounded-full ">
+          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+            <Text className="text-white font-semibold ">Gender</Text>
+            <Text className="text-neutral-300 text-sm">Male</Text>
+          </View>
+          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+            <Text className="text-white font-semibold">Birthday</Text>
+            <Text className="text-neutral-300 text-sm">1964-09-02</Text>
+          </View>
+          <View className="border-r-2 border-r-neutral-400 px-2 items-center">
+            <Text className="text-white font-semibold">known for</Text>
+            <Text className="text-neutral-300 text-sm">Acting</Text>
+          </View>
+          <View className="px-2 items-center">
+            <Text className="text-white font-semibold">Popularity</Text>
+            <Text className="text-neutral-300 text-sm">84.23 %</Text>
+          </View>
+        </View>
+        <View className="my-6 mx-4 space-y-2">
+          <Text className="text-white text-lg">Biography</Text>
+          <Text className="text-neutral-400 tracking-wide">000</Text>
+        </View>
+
+        <MovieList title={"Movies"} hideSeeAll={true} data={personMovies} />
+      </View>
+    </ScrollView>
+  );
+}
